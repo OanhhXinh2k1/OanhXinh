fc7923e08a4183f04c2f2a992f1e737b77e81dbf
OanhhXinh2k1 <125112862+OanhhXinh2k1@users.noreply.github.com>
2023-02-11 12:49:37 +0700

    Add files via upload

---
+++ SQL buổi 2.sql
@@ -0,0 +1,79 @@
+DROP DATABASE IF EXISTS quanly;
+CREATE DATABASE quanly;
+USE quanly;
+
+CREATE TABLE Department(
+DepartmentID	INT,
+DepartmentName	VARCHAR (50)
+);
+
+CREATE TABLE Position (
+PositionID		INT,
+PositionName	VARCHAR(50)
+);
+
+CREATE TABLE Account (
+AccountID		 INT,
+Email			VARCHAR(50),
+Username		VARCHAR(50),
+FullName		VARCHAR(50),
+DepartmentID	INT,
+PositionID		INT,
+PositionID		INT
+);
+
+CREATE TABLE `Group` (
+GroupID			INT,
+GroupName		VARCHAR(50),
+CreatorID		INT,
+CreateDate		VARCHAR(50)
+);
+
+CREATE TABLE GroupAccount (
+GroupID			INT,
+AccountID		INT,
+JoinDate		DATE
+);
+
+CREATE TABLE TypeQuestion (
+TypeID			INT,
+TypeName		VARCHAR(50)
+);
+
+CREATE TABLE CategoryQuestion (
+CategoryID		INT,
+CategoryName	VARCHAR(50)
+);
+
+CREATE TABLE Question (
+QuestionID		INT,
+Content			VARCHAR(50),
+CategoryID		INT,
+TypeID			INT,
+CreatorID		INT,
+CreateDate		VARCHAR(50)
+);
+
+CREATE TABLE Answer (
+AnswerID		INT,
+Content			VARCHAR(50),
+QuestionID		INT,
+isCorrect		VARCHAR(50)
+);
+
+CREATE TABLE Exam (
+ExamID			INT,
+Code			VARCHAR (50),
+Title			VARCHAR(50),
+CategoryID		INT,
+Duration		VARCHAR(50),
+CreatorID		INT,
+CreateDate		DATE
+);
+
+CREATE TABLE ExamQuestion (
+ExamID			INT,
+QuestionID		INT
+);
+
+
